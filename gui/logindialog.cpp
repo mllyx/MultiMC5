88930a6cc4cd9779c54b7d7636ddc767719bed4e
robotbrain <robotbrainify@gmail.com>
2013-10-19 11:56:42 -0400

    Remove unnecessary code

--- gui/logindialog.cpp
+++ gui/logindialog.cpp
@@ -206,8 +206,9 @@ void LoginDialog::userTextChanged ( const QString& user )
 		}
 	}
 
-	if(gotFace) ui->lblFace->setVisible(true);
-	else ui->lblFace->setVisible(false);
+	//if(gotFace) ui->lblFace->setVisible(true);
+	//else ui->lblFace->setVisible(false);
+	ui->lblFace->setVisible(gotFace);
 	blockToggles = false;
 }
 
